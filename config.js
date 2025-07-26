76e8f9a2ac4cf0d3ea1826753fe90ab8a393b4a9
Spike <l-yanlei@hotmail.com>
2025-07-26 19:18:43 +0800

    :wrench: resume resumePath

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // update to your resume.md path
-export const resumePath = path.resolve(__dirname, "../../Dropbox/resume/resume.md")
+export const resumePath = path.resolve(__dirname, "./resume.md")
